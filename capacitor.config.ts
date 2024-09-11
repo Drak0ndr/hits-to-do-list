aa303d08f49604b08cd64fb2a2d8164194f59422
Максим <drakondr.video@gmail.com>
2024-09-11 07:57:18 +0700

    mobile port

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Drak0ndr.hitsToDoList',
+  appName: 'hits-to-do-list',
+  webDir: 'dist'
+};
+
+export default config;
